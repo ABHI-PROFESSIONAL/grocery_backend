d81221ea9f4a2b845034df3762ee4ce941e8f60e
Abhishek Raj <abhishekraj77001@gmail.com>
2023-07-15 22:15:14 +0530

    first commit

---
+++ src/main/java/dev/abhiraz/grocery__backend/GroceryBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.abhiraz.grocery__backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/abhiraz/grocery__backend/GroceryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.abhiraz.grocery__backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
